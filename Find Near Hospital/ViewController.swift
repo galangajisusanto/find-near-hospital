58370a0b10e771267ecba3f33a66920795b5fd49
galangajisusanto <galangajisusanto@gmail.com>
2021-07-12 15:48:13 +0700

    Initial Commit

---
+++ Find Near Hospital/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find Near Hospital
+//
+//  Created by Galang Aji Susanto on 12/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
